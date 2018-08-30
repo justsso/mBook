8ea21b4ed495db27f6bc70d108db8727baf0dd7e
小雅 <528226249@qq.com>
2018-08-30 22:42:39 +0800

    错误处理

---
+++ src/container/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ErrorBoundary extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    componentDidCatch(){
+
+    }
+
+}
+
+
+export default ErrorBoundary
