9718967cacf06a7f089fb1a080f8da33251cbd3f
小雅 <528226249@qq.com>
2018-08-31 23:26:15 +0800

    错误处理

--- src/container/ErrorBoundary/ErrorBoundary.js
+++ src/container/ErrorBoundary/ErrorBoundary.js
@@ -3,12 +3,25 @@ import React, {Component} from 'react';
 class ErrorBoundary extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            error: false,
+            info: ''
+        }
     }
 
-    componentDidCatch(){
-
+    componentDidCatch(err, info){
+        this.setState({
+            error,
+            info
+        })
     }
 
+    render(){
+        if(this.state.error){
+            return <p>{this.state.error.toString()}</p>
+        }
+        return this.props.children
+    }
 }
 
 
